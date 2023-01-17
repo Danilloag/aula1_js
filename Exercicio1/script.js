401ef41d0be366e0e5adc9a556e550dc3cba8e83
Danilloag <115670146+Danilloag@users.noreply.github.com>
2023-01-17 20:36:36 -0300

    Add files via upload

---
+++ Exercicio1/script.js
@@ -0,0 +1,6 @@
+let recebePrimeiroNumero = 10;
+let recebeSegundoNumero = 5;
+
+let soma = recebePrimeiroNumero + recebeSegundoNumero;
+
+console.log(soma);
---
+++ Exercicio2/script.js
@@ -0,0 +1,7 @@
+let recebePrimeiroNumero = Number(prompt('Digite o primeiro número:'));
+
+let recebeSegundoNumero = Number(prompt('Digite o segundo número:'));
+
+let soma = recebePrimeiroNumero + recebeSegundoNumero;
+
+console.log(soma);
